e50ffbd1bdb7df0e993b2b1d8b45acc38ff23518
Mohan-S-Mahajan <mohan.mahajann@gmail.com>
2024-11-25 23:27:53 +0530

    vite config

--- Frontend/Frontend/vite.config.js
+++ Frontend/Frontend/vite.config.js
@@ -1,7 +1,19 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure the output goes to the 'dist' directory
+    emptyOutDir: true, // Clear the dist folder before building
+  },
+  root: '.', // Ensures the root directory is the current directory (where index.html is)
 })
