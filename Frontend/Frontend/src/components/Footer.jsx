53d856f931c7fc07e3c828c3d493907a772b840d
Mohan-S-Mahajan <mohan.mahajann@gmail.com>
2024-12-01 15:26:08 +0530

    footer

--- Frontend/Frontend/src/components/Footer.jsx
+++ Frontend/Frontend/src/components/Footer.jsx
@@ -1,21 +1,11 @@
 import React from 'react';
 import './Footer.css';
 
-const Footer = ({ onAdminPanelClick }) => {
-  const handleAdminClick = (e) => {
-    e.preventDefault(); // Prevent the default behavior of the anchor tag
-    onAdminPanelClick(); // Trigger the passed click handler
-  };
-
+const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-container">
         <p>&copy; 2024 IntegriFIR. All rights reserved.</p>
-      </div>
-      <div className="admin-container">
-        <a href="#" onClick={handleAdminClick} className="admin-link">
-          admin
-        </a>
       </div>
     </footer>
   );
